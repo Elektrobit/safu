a8fca7cd068b70173c4d4345889c0ba649cdb8f0
Friedrich Schwedler <fschwedler@emlix.com>
2023-08-01 13:29:53 +0200

    switch to ConfigureLibrary.cmake
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

---
+++ cmake/ConfigureLibrary.cmake
@@ -0,0 +1,60 @@
+# SPDX-License-Identifier: MIT
+cmake_minimum_required(VERSION 3.21)
+
+include_guard(GLOBAL)
+set(CONFIGURED_LIBRARIES_LIST "")
+
+include(FetchContent)
+set(FETCHCONTENT_QUIET OFF)
+set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE ALWAYS)
+
+function(configureLibrary LIBRARY_NAME LIBRARY_REPOSITORY LIBRARY_TAG)
+  if(${LIBRARY_NAME} IN_LIST CONFIGURED_LIBRARIES_LIST)
+	return()
+  endif()
+  LIST(APPEND CONFIGURED_LIBRARIES_LIST ${LIBRARY_NAME})
+
+  message("#### Configuring external library ${LIBRARY_NAME} (${CMAKE_BUILD_TYPE}) ####")
+
+  message("Searching for ${LIBRARY_NAME}")
+  find_package(${LIBRARY_NAME} QUIET)
+  if(${LIBRARY_NAME}_FOUND)
+    message("Found ${LIBRARY_NAME}")
+    return()
+  endif()
+  message("Did not find ${LIBRARY_NAME}")
+
+  SET(LOCAL_REPO_DIR ${LIBRARY_REPOSITORY})
+
+
+  if (NOT EXISTS ${LIBRARY_REPOSITORY})
+    message("Fetching ${LIBRARY_NAME}")
+
+    FetchContent_Declare(
+      ${LIBRARY_NAME}
+      GIT_REPOSITORY ${LIBRARY_REPOSITORY}
+      GIT_TAG ${LIBRARY_TAG}
+    )
+    FetchContent_MakeAvailable(${LIBRARY_NAME})
+
+    SET(LOCAL_REPO_DIR ${CMAKE_SOURCE_DIR}/build/${CMAKE_BUILD_TYPE}/cmake/_deps/${LIBRARY_NAME}-src/)
+
+    while(NOT EXISTS ${LOCAL_REPO_DIR}/ci/build.sh)
+      execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 0.5)
+    endwhile()
+
+    message("Done fetching")
+  endif()
+
+  message("Building ${LIBRARY_NAME}")
+
+  set(ENV{LOCAL_INSTALL_DIR} ${CMAKE_SOURCE_DIR}/build/${CMAKE_BUILD_TYPE}/dist)
+
+  execute_process(
+  COMMAND ${LOCAL_REPO_DIR}/ci/build.sh ${CMAKE_BUILD_TYPE}
+  WORKING_DIRECTORY "${LOCAL_REPO_DIR}"
+  COMMAND_ECHO STDOUT
+  )
+
+  message("Done building")
+endfunction()
