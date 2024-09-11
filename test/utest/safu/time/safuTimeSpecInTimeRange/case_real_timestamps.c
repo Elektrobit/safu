84582ccc14e2dcf3b9347494524bd5e6d3d2a12d
Friedrich Schwedler <fschwedler@emlix.com>
2024-09-11 13:30:50 +0200

    Add PARAM to all calls to test calls
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- test/utest/safu/time/safuTimeSpecInTimeRange/case_real_timestamps.c
+++ test/utest/safu/time/safuTimeSpecInTimeRange/case_real_timestamps.c
@@ -26,30 +26,39 @@ void safuTestSafuTimeSpecInTimeRangeRealTimestamps(void **state) {
     int ret = clock_gettime(CLOCK_REALTIME, &times[NEWEST]);
     assert_int_equal(ret, 0);
 
+    PARAM("newest >= middle > oldest");
     result = safuTimeSpecInTimeRange(times[NEWEST], times[MIDDLE], times[OLDEST]);
     assert_int_equal(result, true);
 
+    PARAM("{0,0}  middle > oldest");
     result = safuTimeSpecInTimeRange(times[NOTSET], times[MIDDLE], times[OLDEST]);
     assert_int_equal(result, true);
 
+    PARAM("{0,0}   middle !> middle");
     result = safuTimeSpecInTimeRange(times[NOTSET], times[MIDDLE], times[MIDDLE]);
     assert_int_equal(result, false);
 
+    PARAM("{0,0}   middle   {0,0}");
     result = safuTimeSpecInTimeRange(times[NEWEST], times[MIDDLE], times[NOTSET]);
     assert_int_equal(result, true);
 
+    PARAM("newest >= oldest   {0,0}");
     result = safuTimeSpecInTimeRange(times[NEWEST], times[OLDEST], times[NOTSET]);
     assert_int_equal(result, true);
 
+    PARAM("oldest !>= newest   {0,0}");
     result = safuTimeSpecInTimeRange(times[OLDEST], times[NEWEST], times[NOTSET]);
     assert_int_equal(result, false);
 
+    PARAM("newest >= newest > oldest");
     result = safuTimeSpecInTimeRange(times[NEWEST], times[NEWEST], times[OLDEST]);
     assert_int_equal(result, true);
 
+    PARAM("newest >= newest !> newest");
     result = safuTimeSpecInTimeRange(times[NEWEST], times[NEWEST], times[NEWEST]);
     assert_int_equal(result, false);
 
+    PARAM("middle >= oldest !> oldest");
     result = safuTimeSpecInTimeRange(times[MIDDLE], times[OLDEST], times[OLDEST]);
     assert_int_equal(result, false);
 }
